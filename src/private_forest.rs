48a5aa1fff64a2d363c2505a590380fb0dc15173
Ehsan <ehsan6sha@gmail.com>
2023-07-13 04:09:59 -0400

    added sharer core methods

--- src/private_forest.rs
+++ src/private_forest.rs
@@ -1,10 +1,11 @@
-//! This example shows how to add a directory to a private forest (also HAMT) which encrypts it.
-//! It also shows how to retrieve encrypted nodes from the forest using `AccessKey`s.
-
 use chrono::{prelude::*, Utc};
+
 use futures::StreamExt;
+use async_trait::async_trait;
 use libipld::Cid;
 use rand::{rngs::ThreadRng, thread_rng};
+use rand_chacha::ChaCha12Rng;
+use rand_core::SeedableRng;
 use std::{
     fs::File,
     io::{Read, Write},
@@ -14,13 +15,24 @@ use std::{
 };
 use wnfs::{common::Metadata, private::AesKey};
 use wnfs::{
-    common::{utils, BlockStore},
+    common::{utils, BlockStore, CODEC_RAW},
     hamt::Hasher,
     namefilter::Namefilter,
-    private::{AccessKey, PrivateDirectory, PrivateForest, PrivateNode},
+    private::{
+        AccessKey, 
+        PrivateDirectory, 
+        PrivateForest, 
+        PrivateNode,
+        ExchangeKey,
+        PrivateKey,
+        PUBLIC_KEY_EXPONENT,
+        share::{recipient, sharer},
+    },
+    public::{PublicDirectory, PublicLink, PublicNode},
 };
+use rsa::{traits::PublicKeyParts, BigUint, Oaep, RsaPrivateKey, RsaPublicKey};
 
-use anyhow::Result;
+use anyhow::{anyhow, Result};
 use log::trace;
 use sha3::Sha3_256;
 
@@ -78,6 +90,96 @@ impl<'a> PrivateDirectoryHelper<'a> {
         } else {
             Err("PrivateDirectoryHelper not initialized".into())
         }
+    }
+
+    async fn root_dir_setup(store: &impl BlockStore) -> Result<(Rc<PrivateForest>, AccessKey)> {
+        // We generate a new simple example file system:
+        let rng = &mut rand::thread_rng();
+        let forest = &mut Rc::new(PrivateForest::new());
+        let root_dir =
+            &mut PrivateDirectory::new_and_store(Default::default(), Utc::now(), forest, store, rng)
+                .await?;
+
+        // And finally we return the forest and the root directory's access key
+        let access_key = root_dir.as_node().store(forest, store, rng).await?;
+        Ok((Rc::clone(forest), access_key))
+    }
+    async fn setup_seeded_keypair_access(
+        forest: &mut Rc<PrivateForest>,
+        access_key: AccessKey,
+        store: &impl BlockStore,
+        seed: [u8; 32],
+        root_did: &str,
+    ) -> Result<[u8; 32]> {
+        let exchange_keypair = SeededExchangeKey::from_seed(seed.clone())?;
+
+        // Store the public key inside some public WNFS.
+        // Building from scratch in this case. Would actually be stored next to the private forest usually.
+        let public_key_cid = exchange_keypair.store_public_key(store).await?;
+        let mut exchange_root = Rc::new(PublicDirectory::new(Utc::now()));
+        exchange_root
+            .write(
+                &["main".into(), "v1.exchange_key".into()],
+                public_key_cid,
+                Utc::now(),
+                store,
+            )
+            .await?;
+        let exchange_root = PublicLink::new(PublicNode::Dir(exchange_root));
+
+        // The user identity's root DID. In practice this would be e.g. an ed25519 key used
+        // for e.g. UCANs or key usually used for authenticating writes.
+
+        let counter = recipient::find_latest_share_counter(
+            0,
+            1000,
+            &exchange_keypair.encode_public_key(),
+            root_did.into(),
+            forest,
+            store,
+        )
+        .await?
+        .map(|x| x + 1)
+        .unwrap_or_default();
+
+        // Write the encrypted AccessKey into the forest
+        sharer::share::<PublicExchangeKey>(
+            &access_key,
+            counter,
+            root_did,
+            forest,
+            exchange_root,
+            store,
+        )
+        .await?;
+
+        Ok(seed)
+    }
+
+    async fn regain_access_from_seed(
+        forest: &PrivateForest,
+        seed: [u8; 32],
+        root_did: &str,
+        store: &impl BlockStore,
+    ) -> Result<PrivateNode> {
+        // Re-derive keypair
+        let exchange_keypair = SeededExchangeKey::from_seed(seed)?;
+    
+        // Re-load private node from forest
+        let counter = recipient::find_latest_share_counter(
+            0,
+            1000,
+            &exchange_keypair.encode_public_key(),
+            root_did,
+            forest,
+            store,
+        )
+        .await?
+        .unwrap_or_default();
+        let label =
+            sharer::create_share_label(counter, root_did.into(), &exchange_keypair.encode_public_key());
+        let node = recipient::receive_share(label, &exchange_keypair, forest, store).await?;
+        node.search_latest(forest, store).await
     }
 
     async fn init(
@@ -956,6 +1058,51 @@ impl<'a> PrivateDirectoryHelper<'a> {
             .split("/")
             .map(|s| s.to_string())
             .collect()
+    }
+}
+
+struct SeededExchangeKey(RsaPrivateKey);
+
+struct PublicExchangeKey(RsaPublicKey);
+
+impl SeededExchangeKey {
+    pub fn from_seed(seed: [u8; 32]) -> Result<Self> {
+        let rng = &mut ChaCha12Rng::from_seed(seed);
+        let private_key = RsaPrivateKey::new(rng, 2048)?;
+        Ok(Self(private_key))
+    }
+
+    pub async fn store_public_key(&self, store: &impl BlockStore) -> Result<Cid> {
+        store.put_block(self.encode_public_key(), CODEC_RAW).await
+    }
+
+    pub fn encode_public_key(&self) -> Vec<u8> {
+        self.0.n().to_bytes_be()
+    }
+}
+
+#[async_trait(?Send)]
+impl PrivateKey for SeededExchangeKey {
+    async fn decrypt(&self, ciphertext: &[u8]) -> Result<Vec<u8>> {
+        let padding = Oaep::new::<Sha3_256>();
+        self.0.decrypt(padding, ciphertext).map_err(|e| anyhow!(e))
+    }
+}
+
+#[async_trait(?Send)]
+impl ExchangeKey for PublicExchangeKey {
+    async fn encrypt(&self, data: &[u8]) -> Result<Vec<u8>> {
+        let padding = Oaep::new::<Sha3_256>();
+        self.0
+            .encrypt(&mut rand::thread_rng(), padding, data)
+            .map_err(|e| anyhow!(e))
+    }
+
+    async fn from_modulus(modulus: &[u8]) -> Result<Self> {
+        let n = BigUint::from_bytes_be(modulus);
+        let e = BigUint::from(PUBLIC_KEY_EXPONENT);
+
+        Ok(Self(rsa::RsaPublicKey::new(n, e).map_err(|e| anyhow!(e))?))
     }
 }
 
